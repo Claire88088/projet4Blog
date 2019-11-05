b0ebbf61376a274b89b8dabf4bca7a1abd49f4e9
Claire <claire_bouton@hotmail.com>
2019-11-05 16:12:55 +0100

    correction bugs

--- lib/vendors/Model/CommentsManagerPDO.php
+++ lib/vendors/Model/CommentsManagerPDO.php
@@ -17,28 +17,8 @@ class CommentsManagerPDO extends CommentsManager
     
     $comment->setId($this->dao->lastInsertId());
   }
-
-  <?php
-namespace Model;
-
-use \Entity\Comment;
-
-class CommentsManagerPDO extends CommentsManager
-{
-  protected function add(Comment $comment)
-  {
-    $q = $this->dao->prepare('INSERT INTO comments SET news = :news, auteur = :auteur, contenu = :contenu, date = NOW()');
-    
-    $q->bindValue(':news', $comment->news(), \PDO::PARAM_INT);
-    $q->bindValue(':auteur', $comment->auteur());
-    $q->bindValue(':contenu', $comment->contenu());
-    
-    $q->execute();
-    
-    $comment->setId($this->dao->lastInsertId());
-  }
   
-  
+
   public function getListOf($news)
   {
     if (!ctype_digit($news))
