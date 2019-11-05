4d1135edf2cc39755a577c3f968eb3f4d9c07886
Claire <claire_bouton@hotmail.com>
2019-11-05 16:29:51 +0100

    ajout de l'action insert (backend)

--- App/Backend/Modules/News/NewsController.php
+++ App/Backend/Modules/News/NewsController.php
@@ -15,4 +15,47 @@ class NewsController extends BackController
     $this->page->addVar('listeNews', $manager->getList());
     $this->page->addVar('nombreNews', $manager->count());
   }
+
+
+  public function executeInsert(HTTPRequest $request)
+  {
+    if ($request->postExists('auteur'))
+    {
+      $this->processForm($request);
+    }
+    
+    $this->page->addVar('title', 'Ajout d\'une news');
+  }
+  
+  /**
+   * Méthode permettant de traiter le formulaire et d'enregistrer la news dans la BDD
+   * utile à la fois pour insérer une nouvelle news (executeInsert) et modifier une news existante (executeUpdate)
+   */
+  public function processForm(HTTPRequest $request)
+  {
+    $news = new News([
+      'auteur' => $request->postData('auteur'),
+      'titre' => $request->postData('titre'),
+      'contenu' => $request->postData('contenu')
+    ]);
+    
+    // L'identifiant de la news est transmis si on veut la modifier.
+    if ($request->postExists('id'))
+    {
+      $news->setId($request->postData('id'));
+    }
+    
+    if ($news->isValid())
+    {
+      $this->managers->getManagerOf('News')->save($news);
+      
+      $this->app->user()->setFlash($news->isNew() ? 'La news a bien été ajoutée !' : 'La news a bien été modifiée !');
+    }
+    else
+    {
+      $this->page->addVar('erreurs', $news->erreurs());
+    }
+    
+    $this->page->addVar('news', $news);
+  }
 }
--- lib/vendors/Model/NewsManager.php
+++ lib/vendors/Model/NewsManager.php
@@ -25,4 +25,30 @@ abstract class NewsManager extends Manager
    * @return int
    */
   abstract public function count();
+
+  /**
+   * Méthode permettant d'ajouter une news.
+   * @param $news News La news à ajouter
+   * @return void
+   */
+  abstract protected function add(News $news);
+  
+  /**
+   * Méthode permettant d'enregistrer une news.
+   * @param $news News la news à enregistrer
+   * @see self::add()
+   * @see self::modify()
+   * @return void
+   */
+  public function save(News $news)
+  {
+    if ($news->isValid())
+    {
+      $news->isNew() ? $this->add($news) : $this->modify($news);
+    }
+    else
+    {
+      throw new \RuntimeException('La news doit être validée pour être enregistrée');
+    }
+  }
 }
