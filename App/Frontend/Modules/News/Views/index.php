fa3bf013c7b0af12fa2f41a200946d55961607dc
Claire <claire_bouton@hotmail.com>
2019-11-12 10:57:37 +0100

    mise en page index Frontend

--- App/Frontend/Modules/News/Views/index.php
+++ App/Frontend/Modules/News/Views/index.php
@@ -2,7 +2,25 @@
 foreach ($listeNews as $news)
 {
 ?>
-  <h2><a href="news-<?= $news['id'] ?>.html"><?= $news['titre'] ?></a></h2>
-  <p><?= nl2br($news['contenu']) ?></p>
+    <!-- Post -->
+    <article class="post">
+    <header>
+      <div class="title">
+        <h2><a href="single.html"><?= $news['titre'] ?></a></h2>
+        <p></p>
+      </div>
+      <div class="meta">
+        <time class="published" datetime="2015-11-01">Le <?= $news['dateAjout']->format('d/m/Y à H\hi') ?></time>
+      </div>
+    </header>
+
+    <p><?= nl2br($news['contenu']) ?></p>
+    
+    <footer>
+      <ul class="actions">
+        <li><a href="single.html" class="button large">Lire la suite...</a></li>
+      </ul>
+    </footer>
+  </article>
 <?php
 }
